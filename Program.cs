2c03e3aeefa8ea90e7a9f39516e37966f0965d48
fadhilr2 <86996097+fadhilr2@users.noreply.github.com>
2026-03-15 20:01:50 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class KodePos
+{
+    public enum Daerah
+    {
+        Batununggal,
+        Kujangsari,
+        Mengger,
+        Wates,
+        Cijaura,
+        Jatisari,
+        Margasari,
+        Sekejati,
+        Kebonwaru,
+        Maleer
+    }
+    public string getKodePos(Daerah daerah)
+    {
+        string[] tabelKodePos =
+        {
+            "40266",
+            "40287",
+            "40267",
+            "40256",
+            "40287",
+            "40286",
+            "40286",
+            "40286",
+            "40272",
+            "40274"
+        };
+
+        return tabelKodePos[(int)daerah];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kodePos = new KodePos();
+        KodePos.Daerah lokasi1 = KodePos.Daerah.Batununggal;
+        Console.WriteLine(kodePos.getKodePos(lokasi1));
+    }
+}
